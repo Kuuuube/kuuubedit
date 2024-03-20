ac09931cb0fd172e5710f4c39151e71831cf3704
kuuuube <hexagonisalie@gmail.com>
2024-03-20 16:20:01 -0400

    Parse commands and add replace

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,41 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file_path = match args.get(1) {
-        Some(some) => some,
-        None => panic!("Invalid args"),
-    };
-    let mut file = match File::open(file_path) {
-        Ok(ok) => ok,
-        Err(_) => panic!("Failed to open file"),
-    };
+    let file_path = args.get(1).expect("Invalid args");
+    let mut file = File::open(file_path).expect("Failed to open file");
     let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Ok(ok) => ok,
-        Err(_) => panic!("Failed to read file"),
-    };
+    file.read_to_string(&mut contents).expect("Failed to read file");
 
     let commands = get_commands();
-    let re = match Regex::new(&commands.find) {
-        Ok(ok) => ok,
-        Err(_) => panic!("Failed to compile regex"),
-    };
-    let match_test = re.captures_iter(&contents);
 
-    let mut file = match File::create("output.txt") {
-        Ok(ok) => ok,
-        Err(_) => panic!("Failed to create output file"),
-    };
-    for captures in match_test {
-        let first_capture = captures.at(0).unwrap_or_default();
-        file.write(format!("{}\n", first_capture).as_bytes()).unwrap_or_default();
+    let output_filepath = "output.txt";
+
+    match commands.operation {
+        Operation::Find => find(&commands.find,&contents, output_filepath).expect("Failed to complete find"),
+        Operation::Replace => replace(&commands.find, &commands.replace, &contents, output_filepath).expect("Failed to complete replace"),
+        _ => return
     }
+}
+
+fn find(find_regex_string: &str, input_string: &str, output_filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let find_regex_compiled = Regex::new(find_regex_string)?;
+    let regex_match = find_regex_compiled.captures_iter(input_string);
+    let mut output_file: File = File::create(output_filepath)?;
+    for captures in regex_match {
+        let first_capture = captures.at(0).unwrap_or_default();
+        output_file.write(format!("{}\n", first_capture).as_bytes())?;
+    }
+    println!("Find completed successfully");
+    Ok(())
+}
+
+fn replace(find_regex_string: &str, replace_string: &str, input_string: &str, output_filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let find_regex_compiled = Regex::new(&find_regex_string)?;
+    let regex_replace = find_regex_compiled.replace_all(input_string, replace_string);
+    let mut output_file: File = File::create(output_filepath)?;
+    output_file.write(regex_replace.as_bytes())?;
+    println!("Replace completed successfully");
+    Ok(())
 }
 
 fn get_commands() -> Commands {
