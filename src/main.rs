cbe70a2de781874390f80b513530d7cc2a15d576
kuuuube <hexagonisalie@gmail.com>
2024-03-20 11:22:15 -0400

    Cleanup line reading

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use onig::Regex;
 
 fn main() {
-    let regex_input = strip_newlines(read_line());
+    let regex_input = read_line();
     let re = Regex::new(&regex_input).unwrap(); //(?<=(\"|\"1))test(?=\")
     let match_test = re.captures_iter("craietciaestnaciesrn\"test\"aicerntaiecnteain\"1test\"");
     for captures in match_test {
@@ -10,15 +10,11 @@ fn main() {
 }
 
 fn read_line() -> String {
+    print!(":");
     std::io::Write::flush(&mut std::io::stdout()).unwrap();
     let mut buffer = String::default();
     std::io::stdin().read_line(&mut buffer).unwrap_or_default();
+    let len = buffer.trim_end_matches(&['\r', '\n'][..]).len();
+    buffer.truncate(len);
     return buffer;
-}
-
-fn strip_newlines(input_string: String) -> String {
-    let mut_input_string: &mut String = &mut input_string.clone();
-    let len = mut_input_string.trim_end_matches(&['\r', '\n'][..]).len();
-    mut_input_string.truncate(len);
-    return mut_input_string.to_owned();
 }
