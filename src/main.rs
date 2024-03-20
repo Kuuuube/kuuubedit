59928e4063718945e5659c97ffe7859e930698f9
kuuuube <hexagonisalie@gmail.com>
2024-03-20 11:46:53 -0400

    Unwrap less

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,40 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut file = File::open(args.get(1).unwrap()).unwrap();
+    let file_path = match args.get(1) {
+        Some(some) => some,
+        None => panic!("Invalid args"),
+    };
+    let mut file = match File::open(file_path) {
+        Ok(ok) => ok,
+        Err(_) => panic!("Failed to open file"),
+    };
     let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+    match file.read_to_string(&mut contents) {
+        Ok(ok) => ok,
+        Err(_) => panic!("Failed to read file"),
+    };
 
     let regex_input = read_line();
-    let re = Regex::new(&regex_input).unwrap(); //(?<=(\"|\"1))test(?=\")
+    let re = match Regex::new(&regex_input) { //(?<=(\"|\"1))test(?=\")
+        Ok(ok) => ok,
+        Err(_) => panic!("Failed to compile regex"),
+    };
     let match_test = re.captures_iter(&contents);
+
+    let mut file = match File::create("output.txt") {
+        Ok(ok) => ok,
+        Err(_) => panic!("Failed to create output file"),
+    };
     for captures in match_test {
-        println!("{}", captures.at(0).unwrap());
+        let first_capture = captures.at(0).unwrap_or_default();
+        file.write(format!("{}\n", first_capture).as_bytes()).unwrap_or_default();
     }
 }
 
 fn read_line() -> String {
     print!(":");
-    std::io::Write::flush(&mut std::io::stdout()).unwrap();
+    std::io::Write::flush(&mut std::io::stdout()).unwrap_or_default();
     let mut buffer = String::default();
     std::io::stdin().read_line(&mut buffer).unwrap_or_default();
     let len = buffer.trim_end_matches(&['\r', '\n'][..]).len();
