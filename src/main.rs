5f7c7e3e03d257cb7b6d51b151f23130c0dbe48d
kuuuube <hexagonisalie@gmail.com>
2024-03-20 00:28:28 -0400

    Initial regex support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use onig::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let regex_input = strip_newlines(read_line());
+    let re = Regex::new(&regex_input).unwrap(); //(?<=(\"|\"1))test(?=\")
+    let match_test = re.captures_iter("craietciaestnaciesrn\"test\"aicerntaiecnteain\"1test\"");
+    for captures in match_test {
+        println!("{}", captures.at(0).unwrap());
+    }
+}
+
+fn read_line() -> String {
+    std::io::Write::flush(&mut std::io::stdout()).unwrap();
+    let mut buffer = String::default();
+    std::io::stdin().read_line(&mut buffer).unwrap_or_default();
+    return buffer;
+}
+
+fn strip_newlines(input_string: String) -> String {
+    let mut_input_string: &mut String = &mut input_string.clone();
+    let len = mut_input_string.trim_end_matches(&['\r', '\n'][..]).len();
+    mut_input_string.truncate(len);
+    return mut_input_string.to_owned();
 }
