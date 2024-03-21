76eaebc02d4af8700cfd676c101a8812ce902d49
kuuuube <hexagonisalie@gmail.com>
2024-03-21 12:21:58 -0400

    Implemented write, output, quit commands

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,50 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = args.get(1).expect("Invalid args");
     let mut file = File::open(file_path).expect("Failed to open file");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Failed to read file");
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents).expect("Failed to read file");
 
-    let commands = get_commands();
+    loop {
+        let commands = get_commands();
 
-    let output_filepath = "output.txt";
-
-    match commands.operation {
-        Operation::Find => find(&commands.find,&contents, output_filepath).expect("Failed to complete find"),
-        Operation::Replace => replace(&commands.find, &commands.replace, &contents, output_filepath).expect("Failed to complete replace"),
-        _ => return
+        if commands.operation == Operation::Find {
+            find(&commands.find, &file_contents, &commands.output_file).expect("Failed to complete find");
+        } else if commands.operation == Operation::Replace {
+            file_contents = replace(&commands.find, &commands.replace, &file_contents).expect("Failed to complete replace");
+        } else if commands.operation == Operation::Write {
+            write(&file_contents, &commands.output_file).expect("Failed to write file");
+        } else if commands.operation == Operation::Output {
+            output(&file_contents).expect("Failed to output file");
+        } else if commands.operation == Operation::Quit {
+            return;
+        }
     }
 }
 
-fn find(find_regex_string: &str, input_string: &str, output_filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn find(find_regex_string: &str, file_contents: &str, output_filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
     let find_regex_compiled = Regex::new(find_regex_string)?;
-    let regex_match = find_regex_compiled.captures_iter(input_string);
+    let regex_match = find_regex_compiled.captures_iter(file_contents);
     let mut output_file: File = File::create(output_filepath)?;
     for captures in regex_match {
         let first_capture = captures.at(0).unwrap_or_default();
         output_file.write(format!("{}\n", first_capture).as_bytes())?;
     }
-    println!("Find completed successfully");
     Ok(())
 }
 
-fn replace(find_regex_string: &str, replace_string: &str, input_string: &str, output_filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn replace(find_regex_string: &str, replace_string: &str, file_contents: &str) -> Result<String, Box<dyn std::error::Error>> {
     let find_regex_compiled = Regex::new(&find_regex_string)?;
-    let regex_replace = find_regex_compiled.replace_all(input_string, replace_string);
+    Ok(find_regex_compiled.replace_all(file_contents, replace_string))
+}
+
+fn write(file_contents: &str, output_filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut output_file: File = File::create(output_filepath)?;
-    output_file.write(regex_replace.as_bytes())?;
-    println!("Replace completed successfully");
+    output_file.write(file_contents.as_bytes())?;
+    Ok(())
+}
+
+fn output(file_contents: &str) -> Result<(), Box<dyn std::error::Error>> {
+    println!("{}", file_contents);
     Ok(())
 }
 
@@ -49,18 +61,27 @@ fn get_commands() -> Commands {
     match input_split.get(0).unwrap() {
         &"f" => commands.operation = Operation::Find,
         &"r" => commands.operation = Operation::Replace,
+        &"w" => commands.operation = Operation::Write,
+        &"o" => commands.operation = Operation::Output,
+        &"q" => commands.operation = Operation::Quit,
         _ => commands.operation = Operation::None
     }
 
     match (input_split.len(), &commands.operation) {
-        (2, Operation::Find) => {
+        (3, Operation::Find) => {
             commands.find = input_split.get(1).unwrap_or(&"").to_string();
-        }
+            commands.output_file = input_split.get(2).unwrap_or(&"").to_string();
+        },
         (3, Operation::Replace) => {
             commands.find = input_split.get(1).unwrap_or(&"").to_string();
             commands.replace = input_split.get(2).unwrap_or(&"").to_string();
-        }
-        (_, _) => panic!("Incorrect number of command params")
+        },
+        (2, Operation::Write) => {
+            commands.output_file = input_split.get(1).unwrap_or(&"").to_string();
+        },
+        (1, Operation::Output | Operation::Quit) => {},
+        (_, Operation::None) => {},
+        (_, _) => {commands.operation = Operation::None; println!("Incorrect number of command params")}
 
     };
     return commands;
@@ -73,7 +94,7 @@ fn read_line() -> String {
     std::io::stdin().read_line(&mut buffer).unwrap_or_default();
     let len = buffer.trim_end_matches(&['\r', '\n'][..]).len();
     buffer.truncate(len);
-    return buffer;
+    return buffer.trim().to_string();
 }
 
 #[derive(Default)]
@@ -88,7 +109,10 @@ struct Commands {
 pub enum Operation {
     None,
     Find,
-    Replace
+    Replace,
+    Write,
+    Output,
+    Quit
 }
 
 impl Default for Operation {
