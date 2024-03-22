3be77c0a40ed3b4ac3e498088b343f25375cc336
kuuuube <hexagonisalie@gmail.com>
2024-03-22 12:15:06 -0400

    Allow no message and use tt for msg

--- src/macros.rs
+++ src/macros.rs
@@ -1,10 +1,18 @@
 #[macro_export]
 macro_rules! unwrap_or_continue {
-    ($res:expr, $msg:literal) => {
+    ($res:expr, $msg:tt) => {
         match $res {
             Ok(ok) => ok,
             Err(err) => {
                 println!("{}: {}", $msg, err);
+                continue;
+            }
+        }
+    };
+    ($res:expr) => {
+        match $res {
+            Ok(ok) => ok,
+            Err(err) => {
                 continue;
             }
         }
