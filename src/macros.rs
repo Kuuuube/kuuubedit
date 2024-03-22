8999c15b42e80ba3a99b0f21e642e05f78a09f05
kuuuube <hexagonisalie@gmail.com>
2024-03-22 14:50:12 -0400

    Add unwrap or continue for option and cleanup undo logic

--- src/macros.rs
+++ src/macros.rs
@@ -1,20 +1,31 @@
 #[macro_export]
-macro_rules! unwrap_or_continue {
+macro_rules! unwrap_result_or_continue {
     ($res:expr, $msg:tt) => {
         match $res {
             Ok(ok) => ok,
-            Err(err) => {
-                println!("{}: {}", $msg, err);
-                continue;
-            }
+            Err(err) => { println!("{}: {}", $msg, err); continue; }
         }
     };
     ($res:expr) => {
         match $res {
             Ok(ok) => ok,
-            Err(err) => {
-                continue;
-            }
+            Err(err) => { continue; }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! unwrap_option_or_continue {
+    ($res:expr, $msg:tt) => {
+        match $res {
+            Some(some) => some,
+            None => { println!("{}", $msg); continue; }
+        }
+    };
+    ($res:expr) => {
+        match $res {
+            Some(some) => some,
+            None => { continue; }
         }
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,7 @@ fn main() {
     let mut file = File::open(file_path).expect("Failed to open file");
     let mut file_contents = String::new();
     file.read_to_string(&mut file_contents).expect("Failed to read file");
-    let mut previous_file_contents = String::new();
-    let mut undo_available = false;
+    let mut previous_file_contents: Option<String> = None;
 
     loop {
         let commands = match commands::get_commands() {
@@ -23,8 +22,7 @@ fn main() {
         };
 
         if args.undo && commands.destructive {
-            previous_file_contents = file_contents.clone();
-            undo_available = true;
+            previous_file_contents = Some(file_contents.clone());
         }
 
         let find_regex = match Regex::new(&commands.find) {
@@ -33,28 +31,23 @@ fn main() {
         };
 
         if commands.operation == Operation::Find {
-            unwrap_or_continue!(operations::find(find_regex, &file_contents, &commands.output_file), "Failed to complete find");
+            unwrap_result_or_continue!(operations::find(find_regex, &file_contents, &commands.output_file), "Failed to complete find");
         } else if commands.operation == Operation::Replace {
-            file_contents = unwrap_or_continue!(operations::replace(find_regex, &commands.replace, &file_contents), "Failed to complete replace");
+            file_contents = unwrap_result_or_continue!(operations::replace(find_regex, &commands.replace, &file_contents), "Failed to complete replace");
         } else if commands.operation == Operation::ReplaceWrite {
-            file_contents = unwrap_or_continue!(operations::replace(find_regex, &commands.replace, &file_contents),"Failed to complete replace");
-            unwrap_or_continue!(operations::write(&file_contents, &commands.output_file), "Failed to write file");
+            file_contents = unwrap_result_or_continue!(operations::replace(find_regex, &commands.replace, &file_contents),"Failed to complete replace");
+            unwrap_result_or_continue!(operations::write(&file_contents, &commands.output_file), "Failed to write file");
         } else if commands.operation == Operation::Write {
-            unwrap_or_continue!(operations::write(&file_contents, &commands.output_file), "Failed to write file");
+            unwrap_result_or_continue!(operations::write(&file_contents, &commands.output_file), "Failed to write file");
         } else if commands.operation == Operation::Output {
-            unwrap_or_continue!(operations::output(&file_contents), "Failed to output file");
+            unwrap_result_or_continue!(operations::output(&file_contents), "Failed to output file");
         } else if commands.operation == Operation::Undo {
             if !args.undo {
                 println!("Undo is not enabled, pass the `--undo` arg to enable it");
                 continue;
             }
-            if !undo_available {
-                println!("Failed to undo, file history not available");
-                continue;
-            }
-            file_contents = previous_file_contents;
-            previous_file_contents = String::new();
-            undo_available = false;
+            file_contents = unwrap_option_or_continue!(previous_file_contents, "Failed to undo, file history not available");
+            previous_file_contents = None;
         } else if commands.operation == Operation::Quit {
             return;
         }
