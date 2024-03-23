c86273ac8deb766980b52487f5b9ff83bcff2089
kuuuube <hexagonisalie@gmail.com>
2024-03-23 11:48:12 -0400

    Disallow buffers less than 1024 bytes

--- src/args_parser.rs
+++ src/args_parser.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use core::str::FromStr;
 
 /// Powerful headless text editor for processing enormous files
 #[derive(Parser, Debug)]
@@ -9,7 +10,7 @@ pub struct Args {
     pub undo: bool,
 
     /// Sets file buffer size
-    #[arg(short, long, default_value_t = 100000000)]
+    #[arg(short, long, default_value_t = 100000000, value_parser = buffer_size_check)]
     pub buffer: u64,
 
     /// Disables file buffering
@@ -20,6 +21,31 @@ pub struct Args {
     #[arg(value_name = "FILEPATH")]
     pub file: String,
 }
+
+fn buffer_size_check(s: &str) -> Result<u64, String> {
+    number_range(s, 1024, u64::MAX)
+}
+
+//https://github.com/newAM/clap-num/blob/25b0c5d1a58058b1dd82092155f55c198793d6ed/src/lib.rs#L24-L114
+pub fn number_range<T: Ord + PartialOrd + std::fmt::Display>(s: &str, min: T, max: T, ) -> Result<T, String> where T: FromStr, <T as FromStr>::Err: std::fmt::Display {
+    let val = s.parse::<T>().map_err(stringify)?;
+    check_range(val, min, max)
+}
+
+fn check_range<T: Ord + std::fmt::Display>(val: T, min: T, max: T) -> Result<T, String> where T: FromStr, <T as FromStr>::Err: std::fmt::Display {
+    if val > max {
+        Err(format!("exceeds maximum of {max}"))
+    } else if val < min {
+        Err(format!("less than minimum of {min}"))
+    } else {
+        Ok(val)
+    }
+}
+
+fn stringify<T: std::fmt::Display>(e: T) -> String {
+    format!("{e}")
+}
+//
 
 pub fn parse_args() -> Args {
     let mut args = Args::parse();
