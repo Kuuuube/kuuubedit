80004ba79cd8be89aa9bd4bde84047e48571b0cf
kuuuube <hexagonisalie@gmail.com>
2024-03-22 22:40:29 -0400

    Add unescaping of regex replace strings

--- src/commands.rs
+++ src/commands.rs
@@ -22,12 +22,12 @@ pub fn get_commands() -> Option<Commands> {
         },
         Operation::Replace => {
             commands.find = input_split.get(1)?.to_string();
-            commands.replace = input_split.get(2)?.to_string();
+            commands.replace = unescape(input_split.get(2)?);
             commands.destructive = true;
         },
         Operation::ReplaceWrite => {
             commands.find = input_split.get(1)?.to_string();
-            commands.replace = input_split.get(2)?.to_string();
+            commands.replace = unescape(input_split.get(2)?);
             commands.output_file = input_split.get(3)?.to_string();
             commands.destructive = true;
         },
@@ -75,6 +75,15 @@ fn parse_command(command_string: &str) -> Vec<String> {
     return command_split;
 }
 
+#[derive(Default)]
+pub struct Commands {
+    pub operation: Operation,
+    pub find: String,
+    pub replace: String,
+    pub output_file: String,
+    pub destructive: bool
+}
+
 fn read_line() -> String {
     print!(":");
     std::io::Write::flush(&mut std::io::stdout()).unwrap_or_default();
@@ -85,11 +94,87 @@ fn read_line() -> String {
     return buffer.trim().to_string();
 }
 
-#[derive(Default)]
-pub struct Commands {
-    pub operation: Operation,
-    pub find: String,
-    pub replace: String,
-    pub output_file: String,
-    pub destructive: bool
+//https://github.com/BurntSushi/ripgrep/blob/a2e6aec7a4d9382941932245e8854f0ae5703a5e/crates/cli/src/escape.rs
+pub fn unescape(s: &str) -> String {
+    use self::State::*;
+
+    let mut bytes = vec![];
+    let mut state = Literal;
+    for c in s.chars() {
+        match state {
+            Escape => match c {
+                '\\' => {
+                    bytes.push(b'\\');
+                    state = Literal;
+                }
+                'n' => {
+                    bytes.push(b'\n');
+                    state = Literal;
+                }
+                'r' => {
+                    bytes.push(b'\r');
+                    state = Literal;
+                }
+                't' => {
+                    bytes.push(b'\t');
+                    state = Literal;
+                }
+                'x' => {
+                    state = HexFirst;
+                }
+                c => {
+                    bytes.extend(format!(r"\{}", c).into_bytes());
+                    state = Literal;
+                }
+            },
+            HexFirst => match c {
+                '0'..='9' | 'A'..='F' | 'a'..='f' => {
+                    state = HexSecond(c);
+                }
+                c => {
+                    bytes.extend(format!(r"\x{}", c).into_bytes());
+                    state = Literal;
+                }
+            },
+            HexSecond(first) => match c {
+                '0'..='9' | 'A'..='F' | 'a'..='f' => {
+                    let ordinal = format!("{}{}", first, c);
+                    let byte = u8::from_str_radix(&ordinal, 16).unwrap();
+                    bytes.push(byte);
+                    state = Literal;
+                }
+                c => {
+                    let original = format!(r"\x{}{}", first, c);
+                    bytes.extend(original.into_bytes());
+                    state = Literal;
+                }
+            },
+            Literal => match c {
+                '\\' => {
+                    state = Escape;
+                }
+                c => {
+                    bytes.extend(c.to_string().as_bytes());
+                }
+            },
+        }
+    }
+    match state {
+        Escape => bytes.push(b'\\'),
+        HexFirst => bytes.extend(b"\\x"),
+        HexSecond(c) => bytes.extend(format!("\\x{}", c).into_bytes()),
+        Literal => {}
+    }
+    std::str::from_utf8(&bytes).unwrap().to_string()
+}
+
+enum State {
+    /// The state after seeing a `\`.
+    Escape,
+    /// The state after seeing a `\x`.
+    HexFirst,
+    /// The state after seeing a `\x[0-9A-Fa-f]`.
+    HexSecond(char),
+    /// Default state.
+    Literal,
 }
